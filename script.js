3ccb32e29605a5e0999fb96c4d307bfc4a6868e8
carolsampaio2009 <00001147179074sp@al.educacao.sp.gov.br>
2025-08-19 19:40:43 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+  document.getElementById("botao-acessibilidade").addEventListener("click", function () {
+    const opcoes = document.getElementById("opcoes-acessibilidade");
+    opcoes.classList.toggle("ativa");
+  });
+</script>
+
